83a88849e71351cd4fdd809e3c06fae79fcb64b6
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-07-03 10:57:34 +0100

    replication dirver

---
+++ src/lsdtt-drivers/strahler-hpc-zanardo.cpp
@@ -0,0 +1,93 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "../LSDStatsTools.hpp"
+#include "../LSDRaster.hpp"
+#include "../LSDIndexRaster.hpp"
+#include "../LSDShapeTools.hpp"
+#include "../LSDFlowInfo.hpp"
+#include "../LSDJunctionNetwork.hpp"
+#include "../LSDStrahlerLinks.hpp"
+
+int main(int nNumberofArgs, char *argv[])
+{
+
+  //Test for correct input arguments
+	if (nNumberofArgs!=6)
+	{
+		cout << "FATAL ERROR: wrong number of inputs.";
+		exit(EXIT_FAILURE);
+	}
+
+  //get input args
+  string Inpath = argv[1];
+  string Outpath = argv[2];
+  string DEMname = argv[3];
+
+  string DEM_Format = "bil";
+
+  //set boundary conditions
+  vector<string> BoundaryConditions(4, "No Flux");
+
+	//load the DEM
+	LSDRaster DEM(Inpath + DEMname, DEM_Format);
+
+  //Fill
+  float MinSlope = 0.0001;
+  LSDRaster FilledDEM = DEM.fill(MinSlope);
+
+  // get a flow info object
+  LSDFlowInfo FlowInfo(BoundaryConditions, FilledDEM);
+
+  LSDIndexRaster ContributingPixels = FlowInfo.write_NContributingNodes_to_LSDIndexRaster();
+
+  //get the sources
+  vector<int> sources;
+
+  int threshold = 103;  // needs to be 0.09 km2 in pixels
+
+  sources = FlowInfo.get_sources_index_threshold(ContributingPixels, threshold);
+
+  // now get the junction network
+  LSDJunctionNetwork ChanNetwork(sources, FlowInfo);
+  std::cout << "Built junction network" << '\n';
+
+  double Lat = atof(argv[4]);
+  double Long = atof(argv[5]);
+  LSDCoordinateConverterLLandUTM converter;
+
+  double northing, easting;
+  int UTM_zone;
+  bool is_North;
+  FlowInfo.get_UTM_information(UTM_zone, is_North);
+  int eId = 22;             // defines the ellipsiod. This is WGS
+  converter.LLtoUTM_ForceZone(eId, Lat, Long, northing, easting, UTM_zone);
+
+  std::cout << "Searching for nearest channel" << '\n';
+
+  int nearest_node = ChanNetwork.get_nodeindex_of_nearest_channel_for_specified_coordinates(easting, northing, 6, 5, FlowInfo);
+  int nearest_junction = ChanNetwork.get_Junction_of_Node(nearest_node, FlowInfo);
+
+  vector<int> sub_basin_sources = ChanNetwork.get_all_source_nodes_of_an_outlet_junction(nearest_junction);
+
+  LSDJunctionNetwork SubChanNetwork(sub_basin_sources, FlowInfo);
+
+  LSDStrahlerLinks Links(SubChanNetwork, FlowInfo);
+  Links.CalculateTokunagaIndexes(SubChanNetwork, FlowInfo);
+  Links.calculate_lengths(FlowInfo);
+
+  stringstream ss;
+  ss << DEMname << "_" << 1;
+  Links.WriteTokunagaData(Outpath, ss.str());
+
+  stringstream ss2;
+  ss2 << Outpath << "toku_network_" << DEMname << "_" << 1;
+  Links.WriteTokunagaChannelsCSV(SubChanNetwork, ss2.str());
+
+  stringstream ss3;
+  ss3 << Outpath << "full_network_" << DEMname;
+  ChanNetwork.StreamOrderArray_to_WGS84CSV(ss3.str());
+
+}
---
+++ src/lsdtt-drivers/strahler-hpc-zanardo.make
@@ -0,0 +1,24 @@
+CC = g++
+CFLAGS= -c -Wall -O3
+OFLAGS = -Wall -O3
+SOURCES = strahler-hpc-zanardo.cpp \
+    ../LSDIndexRaster.cpp \
+    ../LSDRaster.cpp \
+    ../LSDFlowInfo.cpp \
+    ../LSDShapeTools.cpp \
+    ../LSDStatsTools.cpp \
+		../LSDJunctionNetwork.cpp \
+		../LSDIndexChannel.cpp \
+		../LSDChannel.cpp \
+		../LSDMostLikelyPartitionsFinder.cpp \
+	  ../LSDStrahlerLinks.cpp
+OBJECTS=$(SOURCES:.cpp=.o)
+EXECUTABLE=strahler-hpc-zanardo.out
+
+all: $(SOURCES) $(EXECUTABLE)
+
+$(EXECUTABLE): $(OBJECTS)
+	$(CC) $(OFLAGS) $(OBJECTS) -o $@
+
+.cpp.o:
+	$(CC) $(CFLAGS) $< -o $@
