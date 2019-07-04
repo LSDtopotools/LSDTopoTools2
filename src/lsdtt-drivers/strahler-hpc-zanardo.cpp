79a39a7a865f244fe57ec4e51178bced50cf8e9a
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-07-04 12:44:54 +0100

    getting data for the nearest neighbour analysis

--- src/lsdtt-drivers/strahler-hpc-zanardo.cpp
+++ src/lsdtt-drivers/strahler-hpc-zanardo.cpp
@@ -56,41 +56,42 @@ int main(int nNumberofArgs, char *argv[])
 
   stringstream ss3;
   ss3 << Outpath << "full_network_" << DEMname;
-  ChanNetwork.StreamOrderArray_to_WGS84CSV(ss3.str());
+  ChanNetwork.PrintChannelNetworkToCSV(FlowInfo, ss3.str());
 
-  double Lat = atof(argv[4]);
-  double Long = atof(argv[5]);
-  LSDCoordinateConverterLLandUTM converter;
-
-  double northing, easting;
-  int UTM_zone;
-  bool is_North;
-  FlowInfo.get_UTM_information(UTM_zone, is_North);
-  int eId = 22;             // defines the ellipsiod. This is WGS
-  converter.LLtoUTM_ForceZone(eId, Lat, Long, northing, easting, UTM_zone);
-
-  std::cout << "Searching for nearest channel" << '\n';
-  std::cout << FlowInfo.get_NRows() << '\n';
-  std::cout << FlowInfo.get_NCols() << '\n';
-  std::cout << "------------------" << '\n';
-  int nearest_node = ChanNetwork.get_nodeindex_of_nearest_channel_for_specified_coordinates(easting, northing, 6, 25, FlowInfo);
-  int nearest_junction = ChanNetwork.get_Junction_of_Node(nearest_node, FlowInfo);
-
-  vector<int> sub_basin_sources = ChanNetwork.get_all_source_nodes_of_an_outlet_junction(nearest_junction);
-
-  LSDJunctionNetwork SubChanNetwork(sub_basin_sources, FlowInfo);
-
-  LSDStrahlerLinks Links(SubChanNetwork, FlowInfo);
-  Links.CalculateTokunagaIndexes(SubChanNetwork, FlowInfo);
-  Links.calculate_lengths(FlowInfo);
-
-  stringstream ss;
-  ss << DEMname << "_" << 1;
-  Links.WriteTokunagaData(Outpath, ss.str());
-
-  stringstream ss2;
-  ss2 << Outpath << "toku_network_" << DEMname << "_" << 1;
-  Links.WriteTokunagaChannelsCSV(SubChanNetwork, ss2.str());
+  //
+  // double Lat = atof(argv[4]);
+  // double Long = atof(argv[5]);
+  // LSDCoordinateConverterLLandUTM converter;
+  //
+  // double northing, easting;
+  // int UTM_zone;
+  // bool is_North;
+  // FlowInfo.get_UTM_information(UTM_zone, is_North);
+  // int eId = 22;             // defines the ellipsiod. This is WGS
+  // converter.LLtoUTM_ForceZone(eId, Lat, Long, northing, easting, UTM_zone);
+  //
+  // std::cout << "Searching for nearest channel" << '\n';
+  // std::cout << FlowInfo.get_NRows() << '\n';
+  // std::cout << FlowInfo.get_NCols() << '\n';
+  // std::cout << "------------------" << '\n';
+  // int nearest_node = ChanNetwork.get_nodeindex_of_nearest_channel_for_specified_coordinates(easting, northing, 6, 25, FlowInfo);
+  // int nearest_junction = ChanNetwork.get_Junction_of_Node(nearest_node, FlowInfo);
+  //
+  // vector<int> sub_basin_sources = ChanNetwork.get_all_source_nodes_of_an_outlet_junction(nearest_junction);
+  //
+  // LSDJunctionNetwork SubChanNetwork(sub_basin_sources, FlowInfo);
+  //
+  // LSDStrahlerLinks Links(SubChanNetwork, FlowInfo);
+  // Links.CalculateTokunagaIndexes(SubChanNetwork, FlowInfo);
+  // Links.calculate_lengths(FlowInfo);
+  //
+  // stringstream ss;
+  // ss << DEMname << "_" << 1;
+  // Links.WriteTokunagaData(Outpath, ss.str());
+  //
+  // stringstream ss2;
+  // ss2 << Outpath << "toku_network_" << DEMname << "_" << 1;
+  // Links.WriteTokunagaChannelsCSV(SubChanNetwork, ss2.str());
 
 
 
