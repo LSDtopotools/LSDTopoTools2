e658d6436dc8071381f9c999f8ba5aadd1208fe0
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2021-02-03 17:34:45 +0000

    Updating build file

--- src/build.sh
+++ src/build.sh
@@ -5,10 +5,35 @@ dir=$PWD
 parentdir="$(dirname "$dir")"
 bindir=/bin
 fullbindir=$parentdir$bindir
+
+# Make the binary directory if it doesn't exist
+if [ -d $fullbindir ]
+  then
+    echo "LSDTopoTools binary directory exists!"
+  else
+    echo "LSDTopoTools binary directory doesnt' exist. I'm making one"
+    mkdir $fullbindir
+fi
+
+
 echo The full directory for binary files is:
 echo $fullbindir
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DCMAKE_INSTALL_BINDIR=$fullbindir
+libdir=/lib
+fulllibdir=$parentdir$libdir
+
+# Make the binary directory if it doesn't exist
+if [ -d $fulllibdir ]
+  then
+    echo "LSDTopoTools library directory exists!"
+  else
+    echo "LSDTopoTools library directory doesnt' exist. I'm making one"
+    mkdir $fulllibdir
+fi
+
+
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DCMAKE_INSTALL_BINDIR=$fullbindir -DCMAKE_INSTALL_LIBDIR=$fullbindir
 
 make
 make install
